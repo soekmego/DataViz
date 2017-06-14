f72d12a8b195c1cddb28fb80a755a1df9d7977e9
soekmego <soekmen.goekhan@gmail.com>
2017-06-14 15:13:28 +0200

    mapping codebase

---
+++ source/map.py
@@ -0,0 +1,9 @@
+"""
+Data Visualization Project
+
+Parse data from an CSV or Excel File, render in GeoJSON
+save to a database and visualize in graph form
+
+This project is for educational purposes.
+"""
+
